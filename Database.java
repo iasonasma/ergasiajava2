b6430697407644543c90c4b8b25cb4d7e0fe6ea7
nefeli-v <44037736+nefeli-v@users.noreply.github.com>
2018-11-16 18:58:12 +0200

    Update using Maps

--- Database.java
+++ Database.java
@@ -1,38 +1,41 @@
-package database;
-
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.*;
-
-
 public class Database {
-	public static Scanner input = new Scanner(System.in);
-	public static ArrayList<Field> alist=new ArrayList<Field>();
-	
+
 	public static void main(String[] args) {
 
 	}
 
+	public static void addToList(String name, LinkedList<HashMap> list, ArrayList<String> listData) {
+		HashMap<String, ArrayList<String>> newList = new HashMap<String, ArrayList<String>>();
+		list.add(newList);
+		newList.put(name, listData);
+	}
+
 	public static void menu() {
 		System.out.println("Welcome");
-		System.out.println("** 	Menu  **"); 
+		System.out.println("** 	Menu  **");
 		System.out.println("1. Set a field"); // Field
 		System.out.println("2. Set data ");	   //takeData
-		System.out.println("3. Print data"); //showData
-		System.out.println("4. Delete data");//deleteData
-		System.out.println("5. Change data"); // changeData
-		
+		System.out.println("3.  Print data"); //showData
+		System.out.println("4.  Delete data");//deleteData
+		System.out.print("5.  Change data"); // changeData
+
+		Scanner input = new Scanner(System.in);
+		LinkedList<HashMap> list = new LinkedList<HashMap>();
+		ArrayList<String> listData = new ArrayList<String>();
 
 		int choice = input.nextInt();
 		input.nextLine();
 
 		if (choice == 1) {
 			System.out.println("Please give the name of the field:");
-			String name = input.next();	
-			Field f = new Field(name); 
-			alist.add(f);
-			
-			
+			String name = input.next();
+			Database.addToList(name, list, listData);
+
 
 		} else if (choice == 2) {
 
@@ -45,3 +48,4 @@ public class Database {
 		}
 	}
 }
+
