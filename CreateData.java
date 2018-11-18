f118c747e92627ffaf56f16f3ff7a59cfdb218b1
iasonasma <44037134+iasonasma@users.noreply.github.com>
2018-11-18 21:32:25 +0200

    Create CreateData.java
    Κλάση δημιουργίας αντικειμένων για τα πεδία

---
+++ CreateData.java
@@ -0,0 +1,34 @@
+package database;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class CreateData {
+	public static Scanner input = new Scanner(System.in);
+	public static int counter = 0;
+
+	public static HashMap<Integer, List<String>> values = new HashMap<Integer, List<String>>();
+	static String value;
+	static List<String> valuedata = new ArrayList<String>();
+
+	public static void createData() {
+
+		Datas.fillTheData();
+		int n = Fields.fields.size();
+		int i = 0;
+		List<String> valuedata = new ArrayList<String>();
+		while (i < n) {
+			System.out.println("Give me value for : " + Fields.fields.get(i));
+			String data = input.next();
+			valuedata.add(data);
+			i++;
+		}
+
+		values.put(counter, valuedata);
+		counter++;
+		Database.menu();
+	}
+
+}
