77fc4975e7563a1081de868338c885cd80b13612
stef-06 <32617355+stef-06@users.noreply.github.com>
2018-11-18 19:33:17 +0200

    Changes at class Database
    The database class has the main method and the menu method.

--- Database.java
+++ Database.java
@@ -1,51 +1,41 @@
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.ArrayList;
+package database;
+
 import java.util.Scanner;
-import java.util.*;
+
 public class Database {
 
 	public static void main(String[] args) {
-
-	}
-
-	public static void addToList(String name, LinkedList<HashMap> list, ArrayList<String> listData) {
-		HashMap<String, ArrayList<String>> newList = new HashMap<String, ArrayList<String>>();
-		list.add(newList);
-		newList.put(name, listData);
+		menu();
 	}
 
 	public static void menu() {
-		System.out.println("Welcome");
-		System.out.println("** 	Menu  **");
-		System.out.println("1. Set a field"); // Field
-		System.out.println("2. Set data ");	   //takeData
-		System.out.println("3.  Print data"); //showData
-		System.out.println("4.  Delete data");//deleteData
-		System.out.print("5.  Change data"); // changeData
+		System.out.println("--** Welcome **--");
+		System.out.println("  ** Menu ** ");
+		System.out.println("1. Set a field");
+		System.out.println("2. Set data ");
+		System.out.println("3. Print data");
+		System.out.println("4. Delete data");
+		System.out.print("5. Change data");
+		System.out.println();
 
 		Scanner input = new Scanner(System.in);
-		LinkedList<HashMap> list = new LinkedList<HashMap>();
-		ArrayList<String> listData = new ArrayList<String>();
 
 		int choice = input.nextInt();
 		input.nextLine();
 
 		if (choice == 1) {
-			System.out.println("Please give the name of the field:");
-			String name = input.next();
-			Database.addToList(name, list, listData);
-
+			Fields.createField();
 
 		} else if (choice == 2) {
+			CreateData.createData();
 
 		} else if (choice == 3) {
-
+			Datas.printData();
 		} else if (choice == 4) {
 
 		} else if (choice == 5) {
-
+			ChangeData.changeData();
 		}
+
 	}
 }
-
