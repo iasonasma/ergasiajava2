ecfa92517c3055d03a3d09b8269834e851b146a4
stef-06 <32617355+stef-06@users.noreply.github.com>
2018-11-26 20:04:13 +0200

    Update DeleteData.java
    δημιουργία ξεχωριστών μεθόδων , έλεγχος για τα input του χρήστη (px αν υπάρχει το field που θέλει να διαγράψει) και αλλαγή των id  όταν διαγράφεται ένα id.

--- DeleteData.java
+++ DeleteData.java
@@ -1,6 +1,7 @@
 package database;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,29 +11,69 @@ public class DeleteData {
 	public static void deleteData() {
 		System.out.println("Do you want to delete an ID or just a field? ");
 		String choice = input.next();
-		if (choice.equals("ID")) {
-				System.out.println("Give the value of the ID you want to delete: ");
-				Integer id = input.nextInt();
-				CreateData.values.remove(id);
-				System.out.println("ID deleted.");
+		if ((choice.equals("ID")) || (choice.equals("id"))) {
+			deleteId();
+		} else if (choice.equals("field")) {
+			deleteField();
 		} else {
-			System.out.println("Give me the ID of the form you want to delete: ");
-			int delete = input.nextInt();
-			System.out.println("Which field do you want to delete? ");
-			int index = 0;
-			String deletefield = input.next();
-			for (int i = 0; i < Fields.fields.size(); i++) {
+			System.out.println("Please try again ");
+			deleteData();
+
+		}
+
+	}
+
+	public static void deleteId() {
+		System.out.println("Give the value of the ID you want to delete: ");
+		Integer id = input.nextInt();
+		int done = 0;
+		int size = CreateData.values.size();
+		for (int i = 0; i < CreateData.values.size(); i++) {
+			if (i == id) {
+				done = 1;
+			}
+		}
+		if (done == 1) {
+			for (int i=id; i < CreateData.values.size() - 1; i++){
+			CreateData.values.replace(i,CreateData.values.get(i),CreateData.values.get(i+1));
+			}
+			CreateData.values.remove(size -1 );
+			System.out.println("ID deleted.");
+			Database.menu();
+		} else {
+			System.out.println("The id doesn't exist.Please try again");
+			deleteId();
+		}
+
+	}
+
+
+	public static void deleteField() {
+		System.out.println("Give me the ID of the form you want to delete: ");
+		int delete = input.nextInt();
+		int i = 0 ;
+		System.out.println("Which field do you want to delete? ");
+		int index = 0;
+		String deletefield = input.next();
+		      for (Iterator<String> iterator = Fields.fields.iterator(); iterator.hasNext();) {
+				String num = iterator.next();
 				if ((Fields.fields.get(i).equals(deletefield))) {
 					index = i;
 				}
+				i++;
 			}
-			List<String> test = new ArrayList<String>();
-			test.addAll(CreateData.values.get(delete));
-			test.set(index, "-");
-			CreateData.values.put(delete, test);
-			System.out.println("Field deleted");
+		if (index == 0) {
+			System.out.println("Wrong field name.Please try again");
+			deleteField();
 		}
 
+		List<String> test = new ArrayList<String>();
+		test.addAll(CreateData.values.get(delete));
+		test.set(index, "-");
+		CreateData.values.put(delete, test);
+		System.out.println("Field deleted");
 		Database.menu();
+
 	}
+
 }
