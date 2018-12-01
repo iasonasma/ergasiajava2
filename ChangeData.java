5edbba0609c60c1ba503ae5b8797788a973a76a2
nefeli-v <44037736+nefeli-v@users.noreply.github.com>
2018-12-01 22:37:58 +0200

    Update ChangeData.java
    InputMismatchException
    Check if Database is empty
    Check if id/field exist

--- ChangeData.java
+++ ChangeData.java
@@ -3,34 +3,65 @@ package database;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class ChangeData {
 	static Scanner input = new Scanner(System.in);
 
 	public static void changeData() {
+		if (!CreateData.values.isEmpty()) {
+			System.out.println("Give me the ID of the form you want to change: ");
+			try {
 
-		System.out.println("Give me the ID of the form you wanna change ");
-		int form = input.nextInt();
-		System.out.println("Which field you wanna change?");
-		int index = 0;
-		String formfield = input.next();
-		System.out.println("Give me the new value");
+				int form = input.nextInt();
+				int done = 0;
 
-		String newvalue = input.next();
-		for (int i = 0; i < Fields.fields.size(); i++) {
-			if ((Fields.fields.get(i).equals(formfield))) {
-				index = i;
+				for (int i = 0; i < CreateData.values.size(); i++) {
+					if (i == form) {
+						done = 1;
+					}
+				}
+				if (done == 1) {
+					System.out.println("Which field do you want to change?");
+					int index = 0;
+					String formfield = input.next();
+					int flag = 0;
+					for (int j = 0; j < Fields.fields.size(); j++) {
+						if (Fields.fields.get(j).equals(formfield)) {
+							flag = 1;
+						}
+					}
+					if (flag == 1) {
+						System.out.println("Give me the new value: ");
+
+						String newvalue = input.next();
+						for (int i = 0; i < Fields.fields.size(); i++) {
+							if ((Fields.fields.get(i).equals(formfield))) {
+								index = i;
+							}
+						}
+
+						List<String> test = new ArrayList<String>();
+						test.addAll(CreateData.values.get(form));
+						test.set(index, newvalue);
+						System.out.println(test);
+						CreateData.values.put(form, test);
+						System.out.println("Changes done");
+					} else {
+						System.out.println("The field " + formfield + " does not exist.");
+					}
+				} else {
+					System.out.println("ID doesn't exist.");
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("You need to enter an integer.");
 			}
+		} else {
+			System.out.println("The database is empty - no data available for changes");
 		}
 
-		List<String> test = new ArrayList<String>();
-		test.addAll(CreateData.values.get(form));
-		test.set(index, newvalue);
-		System.out.println(test);
-		CreateData.values.put(form, test);
-		System.out.println("Changes done");
-
 		Database.menu();
+
 	}
 
 }
