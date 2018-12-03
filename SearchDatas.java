9b995ae0cbc1bd31f9c9c26b56c4d5e2841ad9bb
aggeliki92 <44973864+aggeliki92@users.noreply.github.com>
2018-12-03 12:46:05 +0200

    Update SearchDatas.java

--- SearchDatas.java
+++ SearchDatas.java
@@ -1,11 +1,8 @@
 import java.util.Scanner;
-import java.util.HashMap;
-import java.util.List;
 
 public class SearchDatas {
 
 	static Scanner input = new Scanner(System.in);
-	public static HashMap<Integer, List<String>> values = new HashMap<Integer, List<String>>();
 	
 	public static void searchDatas() {
 
@@ -13,12 +10,9 @@ public class SearchDatas {
     
 			System.out.println("Give me the ID of the form you want to find:");
 			int id = input.nextInt();
-			int find = 0;
-
+			
 			for(int i = 0; i < CreateData.values.size(); i++) {
 				if(i == id) {
-					find = 1;
-					id = i;
 					System.out.print("The value is:" + CreateData.values.get(i));
 				}
 			}
