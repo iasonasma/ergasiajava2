87dc6d8c89220e568bee676d69874203ed0ae34c
stef-06 <32617355+stef-06@users.noreply.github.com>
2019-01-04 19:51:57 +0200

    Update ChangeID.java
    checkstyle

--- codeforcheck/ChangeID.java
+++ codeforcheck/ChangeID.java
@@ -1,23 +1,21 @@
+package database;
+
 import java.util.Scanner;
-public class ChangeID{
-
-	public static void changeid()
-	{
-		Scanner inpu = new Scanner(System.in);
-		System.out.println("Give me the ID you wanna change");
-		int id = inpu.nextInt();
-		System.out.println("Give me the name of your primary key");
-		String name = inpu.next();
-		CreateData.values.put(name,CreateData.values.remove(String.valueOf(id)));
-
-
-
-
-
-
-	}
-
-
-
-
+/**
+ * the user can change the primary key.
+ * which is by default the id
+ * */
+public class ChangeID {
+/**
+ * method changeid.
+ **/
+public static void changeid() {
+        Scanner inpu = new Scanner(System.in);
+        System.out.println("Give me the ID you wanna change");
+        int id = inpu.nextInt();
+        System.out.println("Give me the name of your primary key");
+        String name = inpu.next();
+        CreateData.values.put(name,
+                CreateData.values.remove(String.valueOf(id)));
+    }
 }
