5da69804b2bcfca3c979b1a100e258afc51f3436
aggeliki92 <44973864+aggeliki92@users.noreply.github.com>
2019-01-04 21:28:56 +0200

    Update SearchDatas.java

--- SearchDatas.java
+++ SearchDatas.java
@@ -3,21 +3,22 @@ import java.util.Scanner;
 public class SearchDatas {
 
 	static Scanner input = new Scanner(System.in);
-	
+
 	public static void searchDatas() {
 
 		if (!CreateData.values.isEmpty()) {
-    
+
 			System.out.println("Give me the ID of the form you want to find:");
-			int id = input.nextInt();
-			
+			String id = input.next();
+
 			for(int i = 0; i < CreateData.values.size(); i++) {
-				if(i == id) {
-					System.out.println("The value is:" + CreateData.values.get(i));
+				if(Integer.toString(i).equals(id)) {
+					System.out.println("The value is:" + CreateData.values.get(Integer.toString(i)));
 				}
 			}
 		}
 	Database.menu();
 	}
 }
+
 
