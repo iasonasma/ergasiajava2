ea07a4eb2d64df7d18ab2d81b2fc95319edcc8e9
aggeliki92 <44973864+aggeliki92@users.noreply.github.com>
2019-01-04 19:44:23 +0200

    Update SearchValue.java

--- SearchValue.java
+++ SearchValue.java
@@ -10,16 +10,20 @@ public class SearchValue {
 
 			System.out.println("Give me the value you want to find:");
 			String value = input.next();
+			boolean boo = false;
 
 			for(int i = 0; i < CreateData.values.size(); i++) {
 				for(int j = 0; j < CreateData.values.get(i).size(); j++) {
 					if(CreateData.values.get(i).get(j).equals(value)) {
-						System.out.println("The value is here:" + CreateData.values.get(i));
+						boo = true;
+						int id = i;
+						System.out.println("ID:" + id + " and values are: " + CreateData.values.get(i));
 						System.out.println();
 					}
 				}
 			}
 		}
+		System.out.println();
 		Database.menu();
 	}
 }
