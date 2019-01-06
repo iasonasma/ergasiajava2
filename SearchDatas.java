f9c3bee9210a6ef0654b72e2dd5192845bcc426f
stef-06 <32617355+stef-06@users.noreply.github.com>
2019-01-06 16:47:55 +0200

    Update SearchDatas.java
    checkstyle

--- SearchDatas.java
+++ SearchDatas.java
@@ -1,24 +1,28 @@
+package database;
+
 import java.util.Scanner;
-
+/**
+ *class search data.
+ * */
 public class SearchDatas {
+    /**
+     * search data by id.
+     * */
+    protected static void searchDatas() {
 
-	static Scanner input = new Scanner(System.in);
+        if (!CreateData.getValues().isEmpty()) {
+             Scanner input = new Scanner(System.in);
 
-	public static void searchDatas() {
+            System.out.println("Give me the ID of the form you want to find:");
+            int id = input.nextInt();
 
-		if (!CreateData.values.isEmpty()) {
-
-			System.out.println("Give me the ID of the form you want to find:");
-			int id = input.nextInt();
-
-			for(int i = 0; i < CreateData.values.size(); i++) {
-				if(i == id) {
-					System.out.println("The value is:" + CreateData.values.get(i));
-				}
-			}
-		}
-	Database.menu();
-	}
+            for (int i = 0; i < CreateData.getValues().size(); i++) {
+                if (i == id) {
+                    System.out.println("The value is:"
+                            + CreateData.getValues().get(i));
+                }
+            }
+        }
+        Database.menu();
+    }
 }
-
-
