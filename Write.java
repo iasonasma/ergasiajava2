b111da01124dfc2131ae12eefcc4e36e1106820e
stef-06 <32617355+stef-06@users.noreply.github.com>
2019-01-06 16:50:31 +0200

    Update Write.java
    checkstyle

--- Write.java
+++ Write.java
@@ -1,75 +1,82 @@
-import java.util.*;
+package database;
+
 import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.io.FileReader;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Write{
+/**
+ * loads and writes database.
+ * */
+public class Write {
+    /**
+     * load database from a text file.
+     **/
+    public static void writes() {
 
-	public static void writes(){
-				            ArrayList<String> name = new ArrayList<String>();
-				            String line = null;
-				            String again = null;
-try {
+        try {
 
-				            BufferedReader sc = new BufferedReader(new FileReader("datas.txt"));
+            BufferedReader sc = new BufferedReader(new FileReader("datas.txt"));
 
-				            while ((line = sc.readLine()) !=null) {
-				                if (!line.trim().equals("")) {
-									List<String> valuedata = new ArrayList<String>();
+            String line;
+            while ((line = sc.readLine()) != null) {
+                if (!line.trim().equals("")) {
+                    List<String> valuedata = new ArrayList<String>();
 
-				                    if (line.toLowerCase().contains("fields")) {
-										again = line.split("=")[1].trim();
+                    String again;
+                    if (line.toLowerCase().contains("fields")) {
+                        again = line.split("=")[1].trim();
 
-										for(String retval: again.split(","))
-										{
+                        for (String retval : again.split(",")) {
 
-											Fields.fields.add(retval);
-											Fields.fieldscounter++;
+                            Fields.getFields().add(retval);
+                            Fields.setFieldscounter(Fields.getFieldscounter() + 1);
 
-										}
-									}
-									else{
+                        }
+                    } else {
 
-								again = line.split("=")[1].trim();
-								for(String retval: again.split(","))
-								{
-								valuedata.add(retval);
-								}
-								CreateData.values.put(CreateData.counter,valuedata);
-								CreateData.counter++;
-}
-				                }
-				            }
-
-
-						}catch(Exception e){};
-
+                        again = line.split("=")[1].trim();
+                        for (String retval : again.split(",")) {
+                            valuedata.add(retval);
+                        }
+                        CreateData.getValues().put(CreateData.getCounter(), valuedata);
+                        CreateData.setCounter(CreateData.getCounter() + 1);
+                    }
+                }
+            }
 
 
+        } catch (Exception e) {
+        }
 
-	}
+    }
 
+    /**
+     * writes/copy database into a text file.
+     **/
     public static void writedata() {
-		try{
-     		 PrintWriter pr = new PrintWriter(new File("datas.txt"));
-     		 pr.print("fields=");
-     		 for(int i = 0; i < Fields.fields.size();i++)
-     		 {
-	 		pr.print(Fields.fields.get(i) + ",");
-			}
-			pr.println();
-	 		CreateData.values.forEach((key,value) -> {
-			pr.print(key + "=");
-			 for(int i = 0 ; i < value.size(); i++){
+        try {
+            PrintWriter pr = new PrintWriter(new File("datas.txt"));
+            pr.print("fields=");
+            for (int i = 0; i < Fields.getFields().size(); i++) {
+                pr.print(Fields.getFields().get(i) + ",");
+            }
+            pr.println();
+            CreateData.getValues().forEach((key, value) -> {
+                pr.print(key + "=");
+                for (int i = 0; i < value.size(); i++) {
 
-	 		 pr.print(value.get(i) + ",");
-				 }
+                    pr.print(value.get(i) + ",");
+                }
 
-				 pr.println();
-			 });
-		      pr.close();
-		  }catch(Exception e){System.err.println(e);};
-		  Database.menu();
-}
+                pr.println();
+            });
+            pr.close();
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+        Database.menu();
+    }
 }
