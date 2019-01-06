d3f6da55cbda94ad7e2b9a3c901d59b2b23831cf
stef-06 <32617355+stef-06@users.noreply.github.com>
2019-01-06 16:23:55 +0200

    Update CreateData.java
    checkstyle and setters/getters(static because of the static variables)

--- CreateData.java
+++ CreateData.java
@@ -1,39 +1,83 @@
 package database;
 
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
-import java.util.HashMap;
-import java.util.ArrayList;
 
+/**
+ * the user can add datas in the fields.
+ */
 public class CreateData {
-	public static Scanner input = new Scanner(System.in);
-	public static int counter = 0;
 
-	public static HashMap<Integer, List<String>> values = new HashMap<Integer, List<String>>();
-	static String value;
-	static List<String> valuedata = new ArrayList<String>();
+    /**
+     * private variables with accessor methods.
+     * */
+    private static HashMap<Integer, List<String>> values
+            = new HashMap<Integer, List<String>>();
 
-	public static void createData() {
+    private static int counter = 0;
 
-		Datas.fillTheData();
-		int n = Fields.fields.size();
-		int i = 0;
-		List<String> valuedata = new ArrayList<String>();
-		if (!Fields.fields.isEmpty()) {
+    /**
+     * creates Data.
+     */
 
-			while (i < n) {
-				System.out.println("Give me value for : " + Fields.fields.get(i));
-				String data = input.next();
-				valuedata.add(data);
-				i++;
-			}
+    public  static void createData() {
 
-			values.put(counter, valuedata);
-			counter++;
-		} else {
-			System.out.println("There are no fields in order to add data");
-		}
-		Database.menu();
-	}
 
+        Scanner input = new Scanner(System.in);
+        Datas.fillTheData();
+        int n = Fields.getFields().size();
+        int i = 0;
+        List<String> valuedata = new ArrayList<String>();
+        if (!Fields.getFields().isEmpty()) {
+
+            while (i < n) {
+                System.out.println("Give me value for : "
+                        + Fields.getFields().get(i));
+                String data = input.next();
+                valuedata.add(data);
+                i++;
+            }
+
+
+            values.put(counter, valuedata);
+            counter++;
+        } else {
+            System.out.println("There are no fields in order to add data");
+        }
+        Database.menu();
+    }
+
+    /**
+     * the values in a hashmap.
+     * @return values.
+     */
+    public static HashMap<Integer, List<String>> getValues() {
+        return values;
+    }
+
+    /**
+     * setter for values.
+     * @param values
+     **/
+
+    public  void setValues(HashMap<Integer, List<String>> values) {
+
+         CreateData.values = values;
+    }
+    /**
+    * accessor methods for variable counter.
+    **/
+    public static int getCounter() {
+        return counter;
+    }
+    /**
+     * setter for counter.
+     * @param counter
+     **/
+    public static void setCounter(int counter) {
+        CreateData.counter = counter;
+    }
 }
