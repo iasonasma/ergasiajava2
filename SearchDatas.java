e4330bf360ab7bfd52f79620fda656e5c00c379f
nefeli-v <44037736+nefeli-v@users.noreply.github.com>
2019-01-06 23:51:22 +0200

    Update SearchDatas.java
    Add InputMismatchException

--- SearchDatas.java
+++ SearchDatas.java
@@ -1,28 +1,34 @@
 package database;
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
+
 /**
- *class search data.
- * */
+ * class search data.
+ */
 public class SearchDatas {
-    /**
-     * search data by id.
-     * */
-    protected static void searchDatas() {
+	/**
+	 * search data by id.
+	 */
+	protected static void searchDatas() {
 
-        if (!CreateData.getValues().isEmpty()) {
-             Scanner input = new Scanner(System.in);
+		if (!CreateData.getValues().isEmpty()) {
+			Scanner input = new Scanner(System.in);
 
-            System.out.println("Give me the ID of the form you want to find:");
-            int id = input.nextInt();
+			System.out.println("Give me the ID of the form you want to find:");
+			try {
+				int id = input.nextInt();
 
-            for (int i = 0; i < CreateData.getValues().size(); i++) {
-                if (i == id) {
-                    System.out.println("The value is:"
-                            + CreateData.getValues().get(i));
-                }
-            }
-        }
-        Database.menu();
-    }
+				for (int i = 0; i < CreateData.getValues().size(); i++) {
+					if (i == id) {
+						System.out.println("The value is:" + CreateData.getValues().get(i));
+					}
+				}
+			} catch (InputMismatchException e) {
+				System.err.println("Please insert an integer. ");
+				input.next();
+			}
+		}
+		Database.menu();
+	}
 }
