c1e29aebe98df016a17fdeac1afe0ae98c98b490
stef-06 <32617355+stef-06@users.noreply.github.com>
2019-01-06 16:25:22 +0200

    Update Database.java
    checkstyle

--- Database.java
+++ Database.java
@@ -3,79 +3,93 @@ package database;
 import java.util.Scanner;
 import java.util.InputMismatchException;
 
+/**
+ * class Database.
+ * provides the main method
+ */
 public class Database {
-	public  static Scanner input = new Scanner(System.in);
-	public static void main(String[] args) {
-		load();
-	}
-		public static void load()
-	{
-		System.out.println("Do you want to upload fields?");
-		String inputs = input.nextLine();
-		if(inputs.equals("yes"))
-			{
-			Write.writes();
-			menu();
-		}
-		else
-		{
-			Database.menu();
-		}
-	}
-	
-	public static void menu() {
-		try {
-			System.out.println("--** Welcome **--");
-			System.out.println("  ** Menu ** ");
-			System.out.println("1. Set a field");
-			System.out.println("2. Set data ");
-			System.out.println("3. Print data");
-			System.out.println("4. Delete data");
-			System.out.println("5. Change data");
-			System.out.println("6. Search data");
-			System.out.println("7. Search value");
-			System.out.print("8.Save data");
+    /**
+     * load class.
+     * user choose if he/she
+     * wants to upload
+     * existing fields
+     */
+    private static void load() {
+        Scanner input = new Scanner(System.in);
 
-			System.out.println();
+        System.out.println("Do you want to upload fields?");
+        String inputs = input.nextLine();
+        if (inputs.equals("yes")) {
+            Write.writes();
+            menu();
+        } else {
+            menu();
+        }
+    }
 
-			
+    /**
+     * main method of database package.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        load();
+    }
 
-			int choice = input.nextInt();
-			input.nextLine();
+    /**
+     * provides the menu.
+     */
+    public static void menu() {
+        Scanner input = new Scanner(System.in);
+        try {
 
-			if (choice == 1) {
-				Fields.createField();
+            System.out.println("--** Welcome **--");
+            System.out.println("  ** Menu ** ");
+            System.out.println("1. Set a field");
+            System.out.println("2. Set data ");
+            System.out.println("3. Print data");
+            System.out.println("4. Delete data");
+            System.out.println("5. Change data");
+            System.out.println("6. Search data");
+            System.out.println("7. Search value");
+            System.out.println("8. Save data");
 
-			} else if (choice == 2) {
-				CreateData.createData();
+            int choice = input.nextInt();
+            input.nextLine();
 
-			} else if (choice == 3) {
-				Datas.printData();
+            if (choice == 1) {
+                Fields.createField();
 
-			} else if (choice == 4) {
-				DeleteData.deleteData();
-				
-			} else if (choice == 5) {
-				ChangeData.changeData();
-			
-			} else if (choice == 6) {
-				SearchDatas.searchDatas();
-				
-			} else if (choice == 7) {
-				SearchValue.searchValue();
+            } else if (choice == 2) {
+                CreateData.createData();
 
-			} else if (choice == 8) {
-				Write.writedata();
-				
-			}else {
-				System.out.println("Wrong choice.Please try again");
-				menu();
-			}
-		} catch (InputMismatchException e) {
-			System.err.println("Please insert an integer: ");
-			input.next();
-		}
-		menu();
+            } else if (choice == 3) {
+                Datas.printData();
 
-	}
+            } else if (choice == 4) {
+                DeleteData.deleteData();
+
+            } else if (choice == 5) {
+                ChangeData.changeData();
+
+            } else if (choice == 6) {
+                SearchDatas.searchDatas();
+
+            } else if (choice == 7) {
+                SearchValue.searchValue();
+
+            } else if (choice == 8) {
+                Write.writedata();
+
+            } else {
+                System.out.println("Wrong choice.Please try again");
+                menu();
+            }
+        } catch (InputMismatchException e) {
+            System.err.println("Please insert an integer: ");
+            input.next();
+        }
+        menu();
+
+    }
 }
