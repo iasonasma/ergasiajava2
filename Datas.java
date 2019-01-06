6007bb3fa661ceca9d82b73e17507cb12d5d4e85
stef-06 <32617355+stef-06@users.noreply.github.com>
2019-01-06 16:26:04 +0200

    Update Datas.java
    checkstyle

--- Datas.java
+++ Datas.java
@@ -1,41 +1,53 @@
 package database;
 
+/**
+ * class Datas.
+ * provides the methods the user can use
+ * to fill and print the existing deatas.
+ */
 public class Datas {
+    /**
+     * prints the existing datas.
+     */
+    static void printData() {
+        if (!Fields.getFields().isEmpty()) {
+        fillTheData();
+        for (int i = 0; i < Fields.getFields().size(); i++) {
+            System.out.print("     ");
+            System.out.print(Fields.getFields().get(i));
+            System.out.print(" | ");
 
-	public static void printData() {
-		if (!Fields.fields.isEmpty()) {
-			fillTheData();
-			for (int i = 0; i < Fields.fields.size(); i++) {
-				System.out.print("     ");
-				System.out.print(Fields.fields.get(i));
-				System.out.print(" | ");
+        }
+        System.out.println();
+        CreateData.getValues().forEach((key, value) -> {
+            System.out.print("ID:" + key + "->");
+            System.out.print(value);
+            System.out.println();
+        });
+        System.out.println();
+        } else {
+            System.out.println("Empty database");
+        }
 
-			}
-			System.out.println();
-			CreateData.values.forEach((key, value) -> {
-				System.out.print("ID:" + key + "->");
-				System.out.print(value);
-				System.out.println();
-			});
-			System.out.println();
-		} else {
-			System.out.println("Empty database");
-		}
+        Database.menu();
+    }
 
-		Database.menu();
-	}
+    /**
+     * fills the data.
+     */
 
-	public static void fillTheData() {
-		if (!(CreateData.values.isEmpty())) {
-			CreateData.values.forEach((key, value) -> {
-				if (!(value.size() == Fields.fieldscounter)) {
-					do {
-						value.add("-");
-					} while (!(value.size() == Fields.fieldscounter));
-				}
-			});
+    protected static void fillTheData() {
+        if (!(CreateData.getValues().isEmpty())) {
+            CreateData.getValues().forEach((key, value) -> {
+                if (!(value.size() == Fields.getFieldscounter())) {
+                    do {
+                        value.add("-");
+                    } while (!(value.size() == Fields.getFieldscounter()));
+                }
+            });
 
-		}
+        }
 
-	}
+    }
 }
+
