f61ccab77c365773751b5bbb79bc5bf7388d8aaf
stef-06 <32617355+stef-06@users.noreply.github.com>
2019-01-06 17:07:39 +0200

    Create package-info.java

---
+++ package-info.java
@@ -0,0 +1,17 @@
+package database;
+/**
+ * Provides the classes necessary to create a
+ * user friendly  database.
+ * Provides 9 classes.
+ * User can load an existing database
+ * or make a new one.
+ * Then, the user can add fields, datas,
+ * change the datas or delete them.
+ * Database gets updated, expanded and deleted as new information is added.
+ * In this database java program
+ * the primary key is an auto increment ID
+ * which the user can see and use but not change.
+ *
+ * @since 1.0
+ * @author The brogrammers.
+ */
